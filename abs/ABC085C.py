4784326d3a20b9f6d8c5e48a7ed734beccdfe1b4
ca-yoshizaki <yoshizaki.kota@cloud-ace.jp>
2023-04-07 07:32:27 +0900

    ABC085C

--- abs/ABC085C.py
+++ abs/ABC085C.py
@@ -1,8 +1,7 @@
 N, Y = map(int, input().split())
-for i in range(0, N+1):
-    for j in range(0, N+1-i):
-        for k in range(0, N+1-i-j):
-            if i*1000 + j*5000 + k*10000 == Y:
-                print(k, j, i)
-                exit()
-print('-1 -1 -1')
+for i in range(0, N + 1):
+    for j in range(0, N + 1 - i):
+        if i * 10000 + j * 5000 + (N - i - j) * 1000 == Y:
+            print(i, j, N - i - j)
+            exit()
+print("-1 -1 -1")
