71d8799f41699d88dafcf28324be8181bbe899c0
ca-yoshizaki <yoshizaki.kota@cloud-ace.jp>
2023-04-19 08:42:40 +0900

    abc292_A

---
+++ abc292/A/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+
+def solve(S: str):
+    print(S.upper())
+    return
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    S = next(tokens)  # type: str
+    solve(S)
+
+if __name__ == '__main__':
+    main()
