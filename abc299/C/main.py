38b083460e69e2de731aece9edf4df62c88b7795
ca-yoshizaki <yoshizaki.kota@cloud-ace.jp>
2023-05-05 13:51:38 +0900

    もうちょい

--- abc299/C/main.py
+++ abc299/C/main.py
@@ -3,14 +3,26 @@ import sys
 import re
 
 
+def s(N, S):
+    res = -1
+    n = 0
+    for i in range(N):
+        if S[i] == "o":
+            n += 1
+            if n > res:
+                res = n
+        else:
+            n = 0
+    return res
+
+
 def solve(N: int, S: str):
-    res = re.findall(r'(o+)-', S)
-    len_list = list(map(len, res))
-    if len(len_list) >= 1:
-        print(max(len_list))
+    rmax = s(N, S)
+    lmax = s(N, S[::-1])
+    if rmax > lmax:
+        print(rmax)
     else:
-        print(-1)
-    return
+        print(lmax)
 
 
 def main():
@@ -18,10 +30,12 @@ def main():
         for line in sys.stdin:
             for word in line.split():
                 yield word
+
     tokens = iterate_tokens()
     N = int(next(tokens))  # type: int
     S = next(tokens)  # type: str
     solve(N, S)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
