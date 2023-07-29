7e3deeaf83f99c9dbe46f15a75341d26d861a8f0
otokichi3 <yoshizaki.kota@cloud-ace.jp>
2023-07-30 08:04:09 +0900

    C改良

--- abc312/C/main.py
+++ abc312/C/main.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
-N, M = map(int, input().split())
+n, m = map(int, input().split())
 A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-C = [[x, 0] for x in A] + [[x + 1, 1] for x in B]
-C.sort()
-na, nb = 0, M
-for x in C:
-    if x[1] == 0:  # Aから来た
-        na += 1
-    else:  # Bから来た
-        nb -= 1
-    if na >= nb:
-        print(x[0])
-        break
+B = list(map(lambda x: int(x) + 1, input().split()))
+C = sorted(A + B)
+print(C[m-1])
