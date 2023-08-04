f9dc0b6cf96b37e30ec963a4bb81302172e0ef03
otokichi3 <yoshizaki.kota@cloud-ace.jp>
2023-08-04 23:36:14 +0900

    abc311-d

--- abc311/D/main.py
+++ abc311/D/main.py
@@ -1,88 +1,37 @@
 #!/usr/bin/env python3
 from collections import deque
+
 N, M = map(int, input().split())
 S = [list(input()) for _ in range(N)]
-print(N, M, S)
 
 visited = [[False] * M for _ in range(N)]
 q = deque()
-q.append((1, 1)) # 始点(2,2)
+start = (1, 1)  # 始点(2,2)
+q.append(start)
+visited[1][1] = True
+end = set()
+end.add(start)
 d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
-while q: # queueが空になるまで
+while q:
     x, y = q.popleft()
-    visited[x][y] = True
-
-    # 上下左右に突き当たりまで進む
-    # ただし突き当りまでの道がすべて訪問済みなら進まない
-    go = False
-    end = None
-    for i in range(x, N-x): # 下方向
-        nx = x + i
-        if S[nx][y] == ".":
-            if not visited[nx][y]:
-                visited[nx][y] = True
-                go = True
-            if go:
-                end = (nx, y)
-        else:
-            break
-    if go:
-        q.append(end)
-        go = False
-
-    for i in range(x): # 上方向
-        nx = x - i
-        if S[nx][y] == ".":
-            if not visited[nx][y]:
-                visited[nx][y] = True
-                go = True
-            if go:
-                end = (nx, y)
-        else:
-            break
-    if go:
-        q.append(end)
-        go = False
-
-    for i in range(y, M-y): # 右方向
-        ny = y + i
-        if S[x][ny] == ".":
-            if not visited[x][ny]:
-                visited[x][ny] = True
-                go = True
-            if go:
-                end = (x, ny)
-        else:
-            break
-    if go:
-        q.append(end)
-        go = False
-
-    for i in range(y): # 左方向
-        ny = y - i
-        if S[x][ny] == ".":
-            if not visited[x][ny]:
-                visited[x][ny] = True
-                go = True
-            if go:
-                end = (x, ny)
-        else:
-            break
-    if go:
-        q.append(end)
-        go = False
-    print(q)
+    for dx, dy in d:
+        for i in range(1, 200):
+            nx = x + dx * i # +でなく*なのは上もしくは左方向のとき負の値のため
+            ny = y + dy * i
+            if S[nx][ny] == ".":
+                visited[nx][ny] = True
+            else:
+                nx -= dx
+                ny -= dy
+                if (nx, ny) not in end:
+                    end.add((nx, ny))
+                    q.append((nx, ny)) # 壁の直前の座標
+                break
 
 ans = 0
 for i in range(N):
     for j in range(M):
-        if S[i][j] == "#":
-            S[i][j] = "■"
-
         if visited[i][j]:
-            S[i][j] = "x"
             ans += 1
-for i in range(N):
-    print(*S[i])
 print(ans)
