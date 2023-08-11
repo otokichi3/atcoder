2c0e38dbab5d97f4a66bf79956084f327f1bf229
otokichi3 <yoshizaki.kota@cloud-ace.jp>
2023-08-11 17:27:47 +0900

    arc106-b

---
+++ arc106/B/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return "\n".join(f"{r}: {m}" for r, m in self.all_group_members().items())
+
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+uf = UnionFind(n)
+for _ in range(m):
+    c, d = map(int, input().split())
+    uf.unite(c - 1, d - 1)
+
+at = [0] * n
+bt = [0] * n
+for i in range(n):
+    rt = uf.find(i)
+    at[rt] += a[i]
+    bt[rt] += b[i]
+
+if at == bt:
+    print("Yes")
+else:
+    print("No")
